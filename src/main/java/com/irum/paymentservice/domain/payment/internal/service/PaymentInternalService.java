44b29704bffe30b23e61f7ad770517b9edfa6792
hyeonji91 <hjkim1770@gmail.com>
2025-11-06 13:30:15 +0900

    feature #7 : internal service transactional추가

--- src/main/java/com/irum/paymentservice/domain/payment/internal/service/PaymentInternalService.java
+++ src/main/java/com/irum/paymentservice/domain/payment/internal/service/PaymentInternalService.java
@@ -10,10 +10,12 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class PaymentInternalService {
     private final PaymentRepository paymentRepository;
 

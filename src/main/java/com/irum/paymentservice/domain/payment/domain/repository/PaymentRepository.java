c3939e7df259efee95cb5683fbfd7e16085af09a
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2025-11-16 22:20:51 +0900

    refactor #15 : 사용하지 않는 repository함수 삭제

--- src/main/java/com/irum/paymentservice/domain/payment/domain/repository/PaymentRepository.java
+++ src/main/java/com/irum/paymentservice/domain/payment/domain/repository/PaymentRepository.java
@@ -16,7 +16,5 @@ public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Payment p SET p.paymentStatus = 'FAILED' WHERE p.paymentId IN :paymentIds")
     int updateStatusToFailedByIds(@Param("paymentIds") List<UUID> paymentIds);
-
-    @Query("SELECT p.totalDiscountAmount FROM Payment p WHERE p.paymentId = :paymentId")
-    Integer getTotalDiscountByPaymentId(@Param("paymentId") UUID paymentId);
+    
 }

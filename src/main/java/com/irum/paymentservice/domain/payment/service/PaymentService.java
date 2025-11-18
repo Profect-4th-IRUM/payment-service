59766f993e21250d9933de67038dd589099b28a8
hyeonji91 <hjkim1770@gmail.com>
2025-11-18 16:52:18 +0900

    feature #19 : 더티체킹

--- src/main/java/com/irum/paymentservice/domain/payment/service/PaymentService.java
+++ src/main/java/com/irum/paymentservice/domain/payment/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.irum.paymentservice.domain.payment.service;
 
 import com.irum.global.advice.exception.CommonException;
 import com.irum.openfeign.order.enums.OrderStatus;
+import com.irum.openfeign.payment.client.PaymentClient;
 import com.irum.paymentservice.domain.payment.domain.entity.Payment;
 import com.irum.paymentservice.domain.payment.domain.entity.enums.PaymentStatus;
 import com.irum.paymentservice.domain.payment.domain.repository.PaymentRepository;
@@ -27,12 +28,14 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final MemberUtil memberUtil;
     private final OrderAPI orderAPI;
+    private final PaymentStatusService paymentStatusService;
 
     public PaymentResponse createPayment(PaymentRequest request) {
+        log.info("[요청] payment request: {}", request.toString());
         Payment payment =
                 paymentRepository
                         .findById(request.paymentId())
-                        .orElseThrow(() -> new CommonException(PaymentErrorCode.PAYMENT_ERROR));
+                        .orElseThrow(() -> new CommonException(PaymentErrorCode.PAYMENT_NOT_FOUND));
 
         // 접근성 확인
         memberUtil.assertMemberResourceAccess(payment.getMemberId());
@@ -44,7 +47,6 @@ public class PaymentService {
         log.info("Creating payment with payment id {}", payment.getPaymentId());
 
         try {
-
             // 토스 페이먼츠 승인 API호출
             TossPaymentsResponse tossPaymentsResponse =
                     tosspaymentsClient.confirmPayment(request, payment.getAmount());
@@ -58,19 +60,26 @@ public class PaymentService {
                 OrderNum = orderAPI.updateOrderStatusPreparing(
                                 OrderStatus.PREPARING, request.orderId());
             } catch (Exception e) {
-                log.error("Failed to update order status preparing {} message {}", e, e.getMessage());
+                log.error("[Feign] 주문 서비스 통신 에러 {} message {}", e, e.getMessage());
             }
+            log.info("orderNum {}", OrderNum);
 
             return new PaymentResponse(OrderNum, payment.getAmount());
         } catch (FeignException e) {
             // 추후에 Feign client error decoder로 변환하면 좋을 듯
 
             // 상태 업데이트
-            payment.updateStatus(PaymentStatus.FAILED);
+            paymentStatusService.updatePaymentStatusFailed(payment.getPaymentId());
 
-            // order, orderdetail 상태 업데이트, 재고 롤백, 쿠폰 롤백
-            orderAPI.updateOrderStatusFailed(
-                    OrderStatus.FAILED, request.orderId(), request.paymentId());
+            try {
+                // order, orderdetail 상태 업데이트, 재고 롤백, 쿠폰 롤백
+                orderAPI.updateOrderStatusFailed(
+                        OrderStatus.FAILED, request.orderId(), request.paymentId());
+            } catch (Exception orderException) {
+                log.error(
+                        "[에러] exception {} {}",orderException, orderException.getMessage());
+            }
+            log.info("[Feign] orderAPI.updateOrderStatusFailed 완료");
 
             throw new CommonException(PaymentErrorCode.PAYMENT_ERROR);
         }

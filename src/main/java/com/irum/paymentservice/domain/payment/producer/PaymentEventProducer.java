fe36d72a1707fe97705bf3a66670ac84ac554b8f
hyeonji91 <hjkim1770@gmail.com>
2025-11-28 14:10:00 +0900

    feature #27 : payment paid event

---
+++ src/main/java/com/irum/paymentservice/domain/payment/producer/PaymentEventProducer.java
@@ -0,0 +1,43 @@
+package com.irum.paymentservice.domain.payment.producer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.irum.openfeign.order.enums.OrderStatus;
+import com.irum.paymentservice.domain.payment.event.PaymentPaidEvent;
+import com.irum.paymentservice.global.infrastructure.properties.KafkaProperties;
+import com.irum.paymentservice.global.infrastructure.properties.KafkaTopicProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class PaymentEventProducer {
+
+    private final KafkaTemplate<String, PaymentPaidEvent> kafkaTemplate;
+    private final KafkaTopicProperties kafkaTopicProperties;
+
+    public void sendPaymentPaidEvent(OrderStatus orderStatus, UUID orderId){
+        PaymentPaidEvent event = PaymentPaidEvent.from(orderStatus, orderId);
+        String key = event.orderId().toString();
+        ProducerRecord<String, PaymentPaidEvent> record = new ProducerRecord<>(kafkaTopicProperties.paymentPaid(), key, event);
+
+        try {
+            kafkaTemplate.send(record).whenComplete((result, exception) -> {
+                if (exception == null) {
+                    //성공
+                    log.debug("[Success] Sent payment paid event: {}", event);
+                } else {
+                    //실패 처리
+                    log.error("[Error] sending payment paid event : {}", exception.getMessage(), exception);
+                }
+            });
+        } catch (Exception e) {
+            log.error("[Error] sending payment paid event : {}", event, e);
+        }
+    }
+}

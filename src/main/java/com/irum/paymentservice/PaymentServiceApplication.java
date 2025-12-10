53c5ecfbba31973e3d0c6272dd00a526dd7c83b7
Sunwoo Jung <104188728+willjsw@users.noreply.github.com>
2025-12-10 21:21:32 +0900

    [Chore] Spring Cloud 의존성 제거 (#26)
    * chore #25: config import 설정 주석 처리 및 eureka 프로필 import 제외
    
    * chore #25: eureka 의존성 제거 및 config 의존성 주석처리
    
    * chore #25: spotless apply
    
    * chore #25: 환경설정 이관
    
    * feat #25: base-url 환경설정 추가
    
    * chore #25: Kafka 환경변수 변경
    
    * fix #25: Change xml to yaml
    
    * chore #25: MSK ssl 설정 추가
    
    * chore #25: truststore 제거
    
    ---------
    
    Co-authored-by: Bal1oon <ws32159@naver.com>

--- src/main/java/com/irum/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/irum/paymentservice/PaymentServiceApplication.java
@@ -2,12 +2,10 @@ package com.irum.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"com.irum.paymentservice.openfeign"})
-@EnableDiscoveryClient
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
